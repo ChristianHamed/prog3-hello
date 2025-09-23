935d34b0a6dd989ca614c4b2bee0da27e38eace4
Alumno CITEV <alumnog@itson.edu.mx>
2025-09-23 11:29:50 -0700

    dios es vida

---
+++ resources/script.js
@@ -0,0 +1,17 @@
+// Evento que se dispara cuando se termina de cargar el DOM
+//DOM: documento Object Model
+document.addEventListener("DOMContentLoaded", function() {
+
+    //Declaramos una variante del JavaScript que representa el boton
+    var btnOK = document.getElementById("btn-ok")
+
+    //Declaramos un evento click en ese boton
+    btnOK.addEventListener("click", function() {
+        //Declaramos una variable que representa el imput del HTML
+            var name = document.getElementById("input-name");
+
+        //Mostramos una alreta del navegador con el contenido del input
+        alert("Hello World " + name.value);
+    })
+
+})
